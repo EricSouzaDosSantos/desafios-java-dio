792acb9e31ace201d8e1d6bf4eca083d77b12adc
Eric <ericsouza94895@gmail.com>
2025-03-09 23:47:32 -0300

    feat: deleting unnecessary attributes

--- banco/src/main/java/org/dio/model/Client.java
+++ banco/src/main/java/org/dio/model/Client.java
@@ -7,17 +7,8 @@ import java.security.NoSuchAlgorithmException;
 
 public class Client {
     private String name;
-    private String email;
     private String password;
-    private String phone;
-    private String address;
-    private String city;
-    private String state;
-    private String country;
-    private String zipCode;
     private String cpf;
-    private String rg;
-    private String birthDate;
 
 
     public String getName() {
@@ -28,14 +19,6 @@ public class Client {
         this.name = name;
     }
 
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
     public String getPassword() {
         return password;
     }
@@ -44,81 +27,12 @@ public class Client {
         this.password = password;
     }
 
-    public String getPhone() {
-        return phone;
-    }
-
-    public void setPhone(String phone) {
-        this.phone = phone;
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
-    }
-
-    public String getCity() {
-        return city;
-    }
-
-    public void setCity(String city) {
-        this.city = city;
-    }
-
-    public String getState() {
-        return state;
-    }
-
-    public void setState(String state) {
-        this.state = state;
-    }
-
-    public String getCountry() {
-        return country;
-    }
-
-    public void setCountry(String country) {
-        this.country = country;
-    }
-
-    public String getZipCode() {
-        return zipCode;
-    }
-
-    public void setZipCode(String zipCode) {
-        this.zipCode = zipCode;
-    }
-
     public String getCpf() {
-        return ClientService.formatCPF(this.cpf);
+        return this.cpf;
     }
 
     public void setCpf(String cpf) {
-        try {
-            this.cpf = ClientService.validateCPF(cpf);
-        } catch (InvalidCPFException e) {
-            System.out.println("Erro ao validar CPF: " + e.getMessage());
-        } catch (NoSuchAlgorithmException e) {
-            System.out.println("Erro ao cadastrar CPF: " + e.getMessage());
-        }
+        this.cpf = cpf;
     }
 
-    public String getRg() {
-        return rg;
-    }
-
-    public void setRg(String rg) {
-        this.rg = rg;
-    }
-
-    public String getBirthDate() {
-        return birthDate;
-    }
-
-    public void setBirthDate(String birthDate) {
-        this.birthDate = birthDate;
-    }
 }
