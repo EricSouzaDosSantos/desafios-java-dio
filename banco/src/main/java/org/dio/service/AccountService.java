2c35b9058ec9bb6c748ddc4726773050c2aeace2
Eric <ericsouza94895@gmail.com>
2025-03-11 23:47:22 -0300

    feat: add transfer method

--- banco/src/main/java/org/dio/service/AccountService.java
+++ banco/src/main/java/org/dio/service/AccountService.java
@@ -54,18 +54,19 @@ public class AccountService {
         System.out.println("Depósito realizado com sucesso!");
     }
 
-//    public void transfer(Scanner scanner, Account account){
-//        System.out.println("Digite o CPF do destinatário:");
-//        String destinationCPF = scanner.nextLine();
-//
-//        if (client != null) {
-//            Account destinationAccount = accountList.get(client);
-//            System.out.println("Digite o valor da transferência:");
-//            double value = Double.parseDouble(scanner.nextLine());
-//            account.transfer(value, destinationAccount);
-//            System.out.println("Transferência realizada com sucesso!");
-//        } else {
-//            System.out.println("Cliente não encontrado!");
-//        }
-//    }
+    public void transfer(Scanner scanner, Account account) {
+        System.out.println("Digite o CPF do destinatário:");
+        String destinationCPF = scanner.nextLine();
+        Client client = clientService.getClientByCPF(destinationCPF);
+
+        if (client != null) {
+            Account destinationAccount = accountList.get(client);
+            System.out.println("Digite o valor da transferência:");
+            double value = Double.parseDouble(scanner.nextLine());
+            account.transfer(value, destinationAccount);
+            System.out.println("Transferência realizada com sucesso! Saldo atual: " + account.getBalance());
+        } else {
+            System.out.println("Cliente não encontrado!");
+        }
+    }
 }
