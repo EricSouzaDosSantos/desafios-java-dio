2f50d81c95bb126bcaa73396708650710da73e6f
Eric <ericsouza94895@gmail.com>
2025-03-12 23:50:21 -0300

    refactor: delete getBalance method

--- banco/src/main/java/org/dio/interfaces/IAccount.java
+++ banco/src/main/java/org/dio/interfaces/IAccount.java
@@ -3,6 +3,5 @@ package org.dio.interfaces;
 public interface IAccount {
     void deposit(double value);
     void withdraw(double value);
-    double getBalance();
     void transfer(double value, IAccount account);
 }
