060747f496b580fad393a5036bef4ffafad64093
Eric <ericsouza94895@gmail.com>
2025-03-13 23:59:16 -0300

    feat: add withdrawal method, deposit method and finalizing account menu method

--- banco/src/main/java/org/dio/Main.java
+++ banco/src/main/java/org/dio/Main.java
@@ -2,6 +2,7 @@ package org.dio;
 
 import org.dio.exceptions.InvalidCPFException;
 import org.dio.exceptions.InvalidPasswordException;
+import org.dio.model.Account;
 import org.dio.model.Client;
 import org.dio.service.AccountService;
 import org.dio.service.ClientService;
@@ -46,7 +47,7 @@ public class Main {
         }while (optionMenu != 3);
     }
 
-    public static void AccountMenu(){
+    public static void AccountMenu() {
         int optionMenu;
         System.out.println("------------------------Menu Do Banco-------------------------");
         do {
@@ -57,12 +58,61 @@ public class Main {
             System.out.println("4 - Ver Saldo Atual");
             System.out.println("5 - Sair da Conta");
             optionMenu = scanner.nextInt();
+            scanner.nextLine();
 
-            switch (optionMenu){
+            if (clientLogged == null) {
+                System.out.println("Nenhum usuário logado! Faça login primeiro.");
+                return;
+            }
+
+            Account account = accountService.getAccountByClient(clientLogged);
+            if (account == null) {
+                System.out.println("Conta não encontrada para o usuário logado.");
+                return;
+            }
+
+            switch (optionMenu) {
                 case 1:
+                    accountService.transfer(scanner, account);
+                    break;
+                case 2:
+                    withdraw(scanner, account);
+                    break;
+                case 3:
+                    deposit(scanner, account);
+                    break;
+                case 4:
+                    System.out.println("Saldo Atual: " + account.getBalance());
+                    break;
+                case 5:
+                    System.out.println("Saindo da conta...");
+                    clientLogged = null;
+                    break;
+                default:
+                    System.out.println("Opção indisponível");
                     break;
             }
-        }while (optionMenu != 5);
+        } while (optionMenu != 5);
     }
+
+    private static void deposit(Scanner scanner, Account account) {
+        System.out.println("Digite o valor do depósito:");
+        double value = scanner.nextDouble();
+        scanner.nextLine();
+        account.deposit(value);
+        System.out.println("Depósito realizado com sucesso!");
+    }
+
+    private static void withdraw(Scanner scanner, Account account) {
+        System.out.println("Digite o valor do saque:");
+        double value = scanner.nextDouble();
+        scanner.nextLine();
+        if (account.withdraw(value)) {
+            System.out.println("Saque realizado com sucesso!");
+        } else {
+            System.out.println("Saldo insuficiente!");
+        }
+    }
+
 
 }
